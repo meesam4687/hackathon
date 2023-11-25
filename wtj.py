537bdf9e3acb679c4c8cd70db44a3ae931323af7
Meesam <70945820+meesam4687@users.noreply.github.com>
2023-11-25 08:37:27 +0530

    Update wtj.py

--- wtj.py
+++ wtj.py
@@ -1,6 +1,6 @@
 import json
 import time
-def writeToJSON(param1, param2):
+def writeToJSON(param1, param2, param3):
     file_path = "data.json"
     try:
         with open(file_path, 'r') as json_file:
@@ -10,8 +10,17 @@ def writeToJSON(param1, param2):
     new_data = {
         "vehicleOwner": param1,
         "vehicleNumber": param2,
+        "type": param3,
         "timeEntered": time.strftime("%H:%M", time.localtime(time.time()))
     }
     existing_data.append(new_data)
     with open(file_path, 'w') as json_file:
         json.dump(existing_data, json_file, indent=4)
+def readJSON():
+    file_path = "dbe.json"
+    try:
+        with open(file_path, 'r') as json_file:
+            dbe = json.load(json_file)
+            return dbe
+    except FileNotFoundError:
+        return []
